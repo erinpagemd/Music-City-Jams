d5887809f7ed03cc0af72e858478889a49e28512
Erin Page <erinpagemd@yahoo.com>
2015-06-16 23:29:54 -0500

    change sorcery migration since user model exists

--- db/migrate/20150617042416_sorcery_core.rb
+++ db/migrate/20150617042416_sorcery_core.rb
@@ -1,12 +1,8 @@
 class SorceryCore < ActiveRecord::Migration
   def change
-    create_table :users do |t|
-      t.string :email,            :null => false
-      t.string :crypted_password
-      t.string :salt
+    add_column :users, :crypted_password, :string
+    add_column :users, :salt, :string
 
-      t.timestamps
-    end
 
     add_index :users, :email, unique: true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150617041854) do
+ActiveRecord::Schema.define(version: 20150617042416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,8 +29,12 @@ ActiveRecord::Schema.define(version: 20150617041854) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.string   "crypted_password"
+    t.string   "salt"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
