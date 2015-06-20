cb9b59cc8cc19a7fce01299226bad3314f078d50
Erin Page <erinpagemd@yahoo.com>
2015-06-20 18:59:34 -0500

    showing bands and not fans in user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
-    @users = User.all
+    @users = User.where(role: 'band')
   end
 
   def show
