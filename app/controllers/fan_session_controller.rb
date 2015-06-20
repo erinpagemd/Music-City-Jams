9236f8c5c2a5c1c1fb2fa55f59c8ffab5a2488aa
Erin Page <erinpagemd@yahoo.com>
2015-06-20 15:46:43 -0500

    fan sessions controller, create

--- app/controllers/fan_session_controller.rb
+++
@@ -1,4 +0,0 @@
-class FanSessionController < ApplicationController
-  def new
-  end
-end
---
+++ app/controllers/fan_sessions_controller.rb
@@ -0,0 +1,15 @@
+class FanSessionsController < ApplicationController
+  def new
+    @fan = Fan.new
+  end
+
+  def create
+    if @fan = login(params[:fan][:email], params[:fan][:password])
+      redirect_to fan_path(@fan), notice: "Welcome back, #{@fan.name}"
+    else
+      @fan = Fan.new(email: params[:fan][:email])
+      flash.alert = "We could not sign you in. Please check your email/password and try again."
+      render :new
+    end
+  end
+end
