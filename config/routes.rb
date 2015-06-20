13222c4ce52ac5802b6e0134a87a09523428b1af
Erin Page <erinpagemd@yahoo.com>
2015-06-20 15:59:29 -0500

    change routes for sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'fan_sessions/new'
-
-  get 'fan_session/new'
-
   resource :fan_session, only: [:new, :create, :destroy]
   resource :user_session, only: [:new, :create, :destroy]
   resources :fans
