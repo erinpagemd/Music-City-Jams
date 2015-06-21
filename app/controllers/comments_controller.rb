02dad15c8a5a2e4d919af6ff49d72ad1eb8d3ed4
Erin Page <erinpagemd@yahoo.com>
2015-06-20 23:39:47 -0500

    redirect to same gig after adding a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -22,14 +22,11 @@ class CommentsController < ApplicationController
     @comment.user = current_user
     @comment.gig = @gig
 
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    if @comment.save
+      redirect_to user_gig_path(@gig.user, @gig)
+    else
+      flash.alert = "Your comment could not be published. Please correct the errors below."
+      render "gigs/show"
     end
   end
 
