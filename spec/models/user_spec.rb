104854fecc55e8800822a79fb4a0a36df7338200
Erin Page <erinpagemd@yahoo.com>
2015-06-24 16:49:11 -0500

    tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,7 +12,19 @@ RSpec.describe User, type: :model do
     it {should_not validate_presence_of(:password).on(:update)}
     it {should validate_presence_of(:password_confirmation).on(:create)}
     it {should_not validate_presence_of(:password_confirmation).on(:update)}
+    it "should allow valid values for email" do
+      should allow_value("erin@page.com", "a@b.co.uk", "erin+hash@otherness.com").for(:email)
+    end
 
+    describe "should be invalid if email is not formatted correctly" do
+      it {should_not allow_value("erinpagemd.com").for(:email)}
+      it {should_not allow_value("erin@pagecom").for(:email)}
+      it {should_not allow_value("@.com").for(:email)}
+    end
+
+    it "should have a working factory" do
+      Fabricate.build(:user).should be_valid
+    end
 
   end
 end
