ebd74bfda57f7512e874a17704cf6b639a67448f
Erin Page <erinpagemd@yahoo.com>
2015-06-24 17:32:29 -0500

    remove unused files

--- spec/helpers/user_session_helper_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe UserSessionHelper, type: :helper do
-end
--- spec/views/user_gig/show.html.haml_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "user_gig/show.html.haml", type: :view do
-end
