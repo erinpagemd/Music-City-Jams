1fad7654205fe9bda079b55283d15eaeb8c3a408
Erin Page <erinpagemd@yahoo.com>
2015-06-24 16:11:03 -0500

    do not need to test views for comments, as they do not exist

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it {should have_many :comments}
+    it {should have_many :gigs}
+    it {should validate_presence_of(:name)}
+    it {should validate_length_of(:name.is_at_least(3))}
+    it {should validate_presence_of(:email)}
+    it {should validate_uniqueness_of(:email)}
+    it {should validate_confirmation_of(:password)}
+    it {should validate_presence_of(:password).on(:create)}
+    it {should_not validate_presence_of(:password).on(:update)}
+    it {should validate_presence_of(:password_confirmation).on(:create)}
+    it {should_not validate_presence_of(:password_confirmation).on(:update)}
+    
+
+  end
 end
