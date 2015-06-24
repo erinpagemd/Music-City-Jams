8b8bee048eeb18a06b918d7e20f4cf3d39bf93e3
Erin Page <erinpagemd@yahoo.com>
2015-06-24 17:20:32 -0500

    tests for comments model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :gig
   belongs_to :user
-  
+
+  validates :user, :gig, :body, presence:true
 
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  it {should belong_to :gig}
+  it {should belong_to :user}
+  it {should validate_presence_of :body}
+  it {should validate_presence_of :gig}
+  it {should validate_presence_of :user}
+
 end
