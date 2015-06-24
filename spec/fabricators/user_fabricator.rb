fede9f702dd06699a347d1b1e7d50f38cdb98154
Erin Page <erinpagemd@yahoo.com>
2015-06-24 16:52:17 -0500

    rails g fabrication:model User

---
+++ spec/fabricators/user_fabricator.rb
@@ -0,0 +1,11 @@
+Fabricator(:user) do
+  id               1
+  name             "MyString"
+  email            "MyString"
+  created_at       "2015-06-24 16:51:05"
+  updated_at       "2015-06-24 16:51:05"
+  crypted_password "MyString"
+  salt             "MyString"
+  slug             "MyString"
+  role             "MyString"
+end
