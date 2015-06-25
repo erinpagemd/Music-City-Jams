4639520d7319c4f324edc197ee11abe12ac90894
Erin Page <erinpagemd@yahoo.com>
2015-06-25 11:37:44 -0500

    back to RMagick

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@
 
 class ImageUploader < CarrierWave::Uploader::Base
 
-  include CarrierWave::Rmagick
+  include CarrierWave::RMagick
 
   def store_dir
     "development/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
