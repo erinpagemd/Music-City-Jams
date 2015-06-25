d3a12e8cf79802d137162928a16075edec03b291
Erin Page <erinpagemd@yahoo.com>
2015-06-25 14:08:05 -0500

    remove development from store_dir in image_uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
 
   def store_dir
-    "development/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   process :resize_to_limit => [800, 400]
