d43bb985d386801c64c7dd75a0b5e8655a1c2137
Erin Page <erinpagemd@yahoo.com>
2015-06-25 14:42:37 -0500

    correct region in fog carrierwave config

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -9,7 +9,7 @@ else
        :provider               => 'AWS',       # required
        :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],       # required
        :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],       # required
-       :region                 => 'us-standard'  # optional, defaults to 'us-east-1'
+       :region                 => 'us-east-1'  # optional, defaults to 'us-east-1'
   }
    config.fog_directory  = 'musiccityjams-development'                     # required
   end
